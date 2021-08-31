2dbc55a62196cb33afa8055fed8561b5dfb6284d
Vishakha Dikshit <vishakhadikshit@Vishakhas-MacBook-Air.local>
2021-08-31 13:50:19 -0700

    Animation added color tween

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
+//import 'package:animated_text_kit/animated_text_kit.dart';
 
 class Chat extends StatefulWidget {
   @override
   _ChatState createState() => _ChatState();
 }
 
-class _ChatState extends State<Chat> {
+class _ChatState extends State<Chat> with TickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation2;
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      // lowerBound: 1,
+      // upperBound: 100,
+      vsync: this,
+    );
+
+    animation2 =
+        ColorTween(begin: Colors.grey, end: Colors.white).animate(controller);
+
+    controller.forward();
+
+    animation2.addStatusListener((status) {
+      //A StatusListener is called when an animation begins, ends, moves forward, or moves reverse, as defined by AnimationStatus.
+      if (status == AnimationStatus.completed) {
+        //The animation is stopped at the end.
+        controller.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        //The animation is stopped at the beginning.
+        controller.forward();
+      }
+    });
+    controller.addListener(() {
+      setState(() {});
+      //print(controller.value);
+    });
+
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: animation2.value,
       appBar: AppBar(
         leading: Row(children: <Widget>[
           Icon(Icons.arrow_back),
