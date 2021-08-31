7ab53e9b15dc666004ec2b91fc67a8b6bec1a757
Vishakha Dikshit <vishakhadikshit@Vishakhas-MacBook-Air.local>
2021-08-31 12:47:26 -0700

    Final App bar

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -12,14 +12,36 @@ class _ChatState extends State<Chat> {
       appBar: AppBar(
         leading: Row(children: <Widget>[
           Icon(Icons.arrow_back),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
+          Flexible(
             child: CircleAvatar(
-              radius: 1.0,
+              radius: 20.0,
+              backgroundImage: NetworkImage(
+                  "https://4.bp.blogspot.com/-Jx21kNqFSTU/UXemtqPhZCI/AAAAAAAAh74/BMGSzpU6F48/s1600/funny-cat-pictures-047-001.jpg"),
             ),
-          )
+          ),
         ]),
-        title: Text('Itribe Group Chat'),
+        title: Row(children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: Text(
+              'Itribe Group Chat',
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(40, 0, 0, 0),
+            child: Icon(Icons.call),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+            child: Icon(Icons.video_call),
+          ),
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+              child: Icon(Icons.more_vert_sharp),
+            ),
+          ),
+        ]),
       ),
     );
   }
