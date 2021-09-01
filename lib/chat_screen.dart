75320dcf9c8576b5299368afff7f7d32e0b4fae7
Vishakha Dikshit <vishakhadikshit@Vishakhas-MacBook-Air.local>
2021-09-01 12:31:45 -0700

    firestore dependency

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 //import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 late User loggedUser; //firebaseuser
 late bool isMe;
