a08bffa47fdfad51bfc778c686e1f3a3cc231cab
Vishakha Dikshit <vishakhadikshit@Vishakhas-MacBook-Air.local>
2021-09-01 01:25:06 -0700

    Making one genral button

---
+++ lib/registration_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:itribe23/chat_screen.dart';
+import 'package:itribe23/components/roundedbutton.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
